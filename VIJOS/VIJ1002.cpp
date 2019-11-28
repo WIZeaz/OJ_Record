584e9f50476b79b638235917aae782a527e0c637
WIZeaz <764696642@qq.com>
2019-11-28 18:27:28 +0800

    Update VIJ1002.cpp

--- VIJOS/VIJ1002.cpp
+++ VIJOS/VIJ1002.cpp
@@ -1,48 +1,45 @@
 #include <iostream>
+#include <cstring>
 #include <algorithm>
-#include <queue>
 using namespace std;
-long stone[201];
-const long INF=0x3f3f3f3f;
-queue <long> d;
-int main()
-{
-	long i,k,j,l,s,t,st,to,m,Min;
-	
-	cin>>l>>s>>t>>m;
-	for (i=1;i<=m;i++) cin>>stone[i];
-	sort(stone+1,stone+m+1);
-	st=1;
-	while (stone[st]<s) st++;
-		
-	memset(d,127,sizeof(d));
-	d[0]=0;
-	if (stone[st]<=100) to=stone[st]-1;
-	else to=100;
-	if (to<=t){
-		for (i=s;i<=to;i++) d[i]=0;
-	} else {
-		for (i=s;i<=t;i++) d[i]=0;
-		for (i=t+1;i<=to;i++)
-			for (j=i-t;j<=i-s;j++)
-				d[i%(t+1)]=min(d[i%(t+1)],d[j%(t+1)]);
-	}
-	
-
-	stone[m+1]=stone[m]+101;
-	for (k=st;k<=m;k++){
-		for (j=stone[k]-t;j<=stone[k]-s;j++)
-			if (j>=0) d[stone[k]%(t+1)]=min(d[stone[k]%(t+1)],d[j%(t+1)]);
-		d[stone[k]%(t+1)]++;
-		if (stone[k]+100>=stone[k+1]) to=stone[k+1]-1;
-		else to=stone[k]+100;	
-		for (i=stone[k]+1;i<=to;i++){
-			for (j=i-t;j<=i-s;j++)
-				if (j>=0) d[i%(t+1)]=min(d[i%(t+1)],d[j%(t+1)]);
+int a[100001];
+int d[100001];
+int stones[100001];
+int main(){
+	int L,l,r,m;
+	scanf("%d%d%d%d",&L,&l,&r,&m);
+	if (l==r){
+		int ans=0;
+		for (int i=0;i<m;++i){
+			int pos;
+			scanf("%d",&pos);
+			if (pos%l==0) ++ans;
 		}
+		printf("%d",ans);
+	} else {
+		memset(d,0x3f,sizeof(d));
+		memset(a,0,sizeof(a));
+		int pos=0;
+		int bound=r*r;
+		for (int i=0;i<m;++i) scanf("%d",&stones[i]);
+		sort(stones,stones+m);
+		if (stones[0]>=bound) pos=bound;
+		else pos=stones[0];
+		a[pos]=1;
+		for (int i=1;i<m;++i){
+			pos+=min(bound,stones[i]-stones[i-1]);
+			a[pos]=1;
+		}
+		pos+=min(L-stones[m-1],bound);
+		L=pos;
+		d[0]=0;
+		for (int i=1;i<=L+r;++i){
+			for (int j=max(i-r,0);j<=i-l;++j)
+				d[i]=min(d[i],d[j]+a[i]);
+		}
+		int ans=d[L];
+		for (int i=L+1;i<=L+r;++i) ans=min(ans,d[i]);
+		printf("%d",ans);
 	}
-	Min=INF;
-	for (i=1;i<=100;i++)
-		Min=min(Min,d[i]);
-	cout<<Min;
+	return 0;
 }
